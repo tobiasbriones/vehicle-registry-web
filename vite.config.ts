379117452689e6fcf0c0594d94924e3ea98413a3
Tobias Briones <tbriones@mathswe.com>
2024-11-03 14:55:44 -0600

    Setup path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@
 // This file is part of https://github.com/tobiasbriones/vehicle-registry-web
 
 import react from "@vitejs/plugin-react-swc";
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
 // @ts-expect-error They must fix this dev dep to support its types correctly
 import eslint from "vite-plugin-eslint";
@@ -14,4 +15,10 @@ export default defineConfig({
         // eslint-disable-next-line @typescript-eslint/no-unsafe-call
         eslint(),
     ],
+    resolve: {
+        alias: {
+            "@": resolve(__dirname, "src"),
+            "@app": resolve(__dirname, "src"),
+        },
+    },
 });
