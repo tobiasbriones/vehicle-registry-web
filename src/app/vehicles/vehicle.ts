ce2f1465fb83fc018e912716642e195d0d6794ff
Tobias Briones <tbriones@mathswe.com>
2024-11-03 20:39:05 -0600

    Add mod vehicle
    It models a vehicle entity.

---
+++ src/app/vehicles/vehicle.ts
@@ -0,0 +1,9 @@
+// Copyright (c) 2024 Tobias Briones. All rights reserved.
+// SPDX-License-Identifier: MIT
+// This file is part of https://github.com/tobiasbriones/vehicle-registry-web
+
+export type Vehicle = {
+    number: string,
+    brand: string,
+    model: string,
+}
