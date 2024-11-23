b00906e09b3c1e3f77145759da42cf5e03fc5c24
Tobias Briones <tbriones@mathswe.com>
2024-11-23 13:40:51 -0600

    Add section vehicle-registry to landing page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,42 +2,38 @@
 // SPDX-License-Identifier: MIT
 // This file is part of https://github.com/tobiasbriones/vehicle-registry-web
 
-import viteLogo from "/vite.svg";
-import { useState } from "react";
-import reactLogo from "@app/assets/react.svg";
+import { Button } from "primereact/button";
 import "@app/App.css";
 
 function App() {
-    const [ count, setCount ] = useState(0);
+    return <>
+        <div className="landing-page">
+            <VehicleRegistry />
+        </div>
+    </>;
+}
 
-    return (
-        <>
-            <div>
-                <a rel="noreferrer" href="https://vite.dev" target="_blank">
-                    <img src={ viteLogo } className="logo" alt="Vite logo" />
-                </a>
-                <a rel="noreferrer" href="https://react.dev" target="_blank">
-                    <img
-                        src={ reactLogo }
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={ () => { setCount((count) => count + 1); } }>
-                    count is { count }
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
+function VehicleRegistry() {
+    return <>
+        <section
+            className="vehicle-registry flex flex-column align-items-center justify-content-center text-center"
+        >
+            <h1>
+                Welcome to Vehicle Registry
+            </h1>
+
+            <p className="description">
+                A powerful, modern solution for managing vehicles,
+                drivers, and tracking real-time entry
+                and exit logs.
             </p>
-        </>
-    );
+
+            <Button
+                className="getting-started p-button-raised p-button-rounded p-button-primary"
+                label="Getting Started"
+            />
+        </section>
+    </>;
 }
 
 export default App;
