2bcdea2d418d6f9d2685a4cd35a65b3dcb2571ad
Tobias Briones <tbriones@mathswe.com>
2024-11-24 21:32:56 -0600

    Set project version 0.1.0

--- src/info.ts
+++ src/info.ts
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MIT
 // This file is part of https://github.com/tobiasbriones/vehicle-registry-web
 
-export const APP_SEMVER = "0.1.0-dev";
+export const APP_SEMVER = "0.1.0";
 export const APP_NAME = "Vehicle Registry Web";
 export const APP_VERSION = `v${ APP_SEMVER }`;
 export const APP_VERSION_LABEL = `${ APP_NAME } ${ APP_VERSION }`;
